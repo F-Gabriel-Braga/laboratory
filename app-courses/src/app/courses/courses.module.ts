64cd3fa5a3cadc5c2a22d93601d6fc1b00e5dfcd
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-06-06 23:29:11 -0300

    app-courses

---
+++ app-courses/src/app/courses/courses.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+import { SharedModule } from '../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    AppMaterialModule,
+    SharedModule
+  ],
+  exports: [
+    CoursesComponent
+  ]
+})
+export class CoursesModule { }
